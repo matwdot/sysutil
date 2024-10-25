7c161e5e9c9211abefa799c539bd99f0f9630a43
matwdot <matheusw.linux@gmail.com>
2024-10-25 17:53:00 -0300

    Melhorias na função create_alias

--- func/utils/create_alias.sh
+++ func/utils/create_alias.sh
@@ -17,25 +17,35 @@
 
 create_alias() {
   ALIAS_NAME="sysutil"
-  ALIAS_CMD="~/sysutil/sysutil.sh"
+  ALIAS_CMD="(cd ~/sysutil && ./sysutil.sh)"
   ALIAS_LINE="alias $ALIAS_NAME=\"$ALIAS_CMD\""
+  PATH_LINE='export PATH="$PATH:$HOME/sysutil"'
 
-  # Verifica se o alias já existe no .bashrc
-  if grep -Fxq "alias $ALIAS_NAME=\"$ALIAS_CMD\"" ~/.bashrc; then
-    echo "Alias '$ALIAS_NAME' já está presente no .bashrc"
-    read -p ""
-  else
-    # Adiciona o alias ao .bashrc
-    echo -e "\n# Alias sysutil" >>~/.bashrc
-    echo -e "\n$ALIAS_LINE" >>~/.bashrc
+  for RC_FILE in ~/.bashrc ~/.zshrc; do
+    # Verifica se o alias já existe no arquivo de configuração
+    if ! grep -Fxq "$ALIAS_LINE" "$RC_FILE"; then
+      echo -e "\n# Alias sysutil" >>"$RC_FILE"
+      echo "$ALIAS_LINE" >>"$RC_FILE"
+      echo "Alias '$ALIAS_NAME' adicionado ao $RC_FILE com sucesso!"
+    else
+      echo "Alias '$ALIAS_NAME' já está presente no $RC_FILE"
+    fi
 
-    echo "Adicionado com sucesso!"
-    read -p ""
+    # Adiciona o diretório ao PATH se não estiver presente
+    if ! grep -Fxq "$PATH_LINE" "$RC_FILE"; then
+      echo -e "\n# Adiciona sysutil ao PATH" >>"$RC_FILE"
+      echo "$PATH_LINE" >>"$RC_FILE"
+      echo "Diretório '~/sysutil' adicionado ao PATH no $RC_FILE com sucesso!"
+    else
+      echo "O diretório '~/sysutil' já está no PATH no $RC_FILE"
+    fi
+  done
 
-    # Recarrega o .bashrc para atualizar as alterações
-    source ~/.bashrc
-  fi
+  # Recarrega os arquivos de configuração
+  source ~/.bashrc 2>/dev/null
+  source ~/.zshrc 2>/dev/null
 }
+
 
 # Executar função
 create_alias
