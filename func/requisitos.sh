329596dbb46ae5298f973297733cc3e382ecf513
MatheusWesley <matheus.tarra@gmail.com>
2025-02-08 18:37:20 -0300

    melhorias na função de requisitos
    adicionei a biblioteca libnotify que é reponsável por exibir
    notificações via pop-up na tela.

--- func/requisitos.sh
+++ func/requisitos.sh
@@ -28,4 +28,23 @@ requisitos() {
   else
     echo -e "${GREEN}dep: curl -> OK${NC}"
   fi
+  # libnotify
+  if ! command -v notify-send &>/dev/null; then
+    echo "Instalando libnotify"
+    sudo apt update && sudo apt install -y libnotify-bin
+    if [ $? -eq 0 ]; then
+      echo "O notify-send foi instalado com sucesso."
+    else
+      echo "Falha na instalação do notify-send."
+      exit 1
+    fi
+  else
+    echo -e "${GREEN}dep: curl -> OK${NC}"
+    echo -e "${GREEN}dep: libnotify -> OK${NC}"
+  fi
+  sleep 2
+
 }
+
+# Execução da função
+requisitos
